00e642ec5088aa424aead9a137c579f424790075
Ritesh-patra <patrasagarika654@gmail.com>
2025-11-04 15:13:56 +0530

    all fix

--- backend/inngest/index.js
+++ backend/inngest/index.js
@@ -58,7 +58,7 @@ const releaseSeatsAndDeleteBooking = inngest.createFunction(
   { id: "release-seats-delete-booking" },
   { event: "app/checkpayment" },
   async ({ event, step }) => {
-    const tenMinutesLater = new Date(Date.now() + 10 * 60 * 10000);
+    const tenMinutesLater = new Date(Date.now() + 5 * 60 * 1000);
     await step.sleepUntil("wait-for-10-minutes", tenMinutesLater);
 
     await step.run("check-payment-status", async () => {
